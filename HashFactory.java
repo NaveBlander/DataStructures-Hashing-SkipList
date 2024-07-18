3bc37939f226969129f9c126524bfbd11a9a8bcc
Nave Blander <162339225+NaveBlander@users.noreply.github.com>
2024-07-18 14:48:25 +0300

    Delete HashFactory.java

--- HashFactory.java
+++
@@ -1,7 +0,0 @@
-public interface HashFactory<K> {
-    /**
-     * @param capacity - The capacity of the hash table using the expected hash function.
-     * @return A randomly chosen hash function wrapped in a HashFunctor class.
-     */
-    public HashFunctor<K> pickHash(int k);
-}
